506d031144287a59dba4b954923dab008c49a455
Max <91740438+maxime-hadj@users.noreply.github.com>
2021-12-06 16:19:05 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 
 $db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "moduleconnexion";
+//$db_user = "root";
+//$db_password = "";
+//$db_name = "moduleconnexion";
+
+$db_user = "maxime-hadj";
+$db_password = "Telecaster0603";
+$db_name = "maxime-hadj_moduleconnexion";
+
 
 try{
     $db = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
